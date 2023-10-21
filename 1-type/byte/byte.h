180c18273b41fe1dded4004944a7e20ccb19988c
Michail <vonamorm@mail.ru>
2023-10-21 19:47:02 +0300

    a

--- 1-type/byte/byte.h
+++ 1-type/byte/byte.h
@@ -1,13 +1,4 @@
 unsigned char max_byte(unsigned short number) {
-        int num = int(max_byte);
-    int ans;
-    int num_massive[4*num] = {0};
-    int i = 0;
-    while (num > 0)
-    {
-        num_massive[i] = num%2;
-        num = num/2;
-        i=i+1;
-    }
+
     return 0; // Not implementation
 }
