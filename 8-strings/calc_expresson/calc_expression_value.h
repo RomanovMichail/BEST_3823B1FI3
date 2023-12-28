a3a983154c8ec8d223627ea8fbcea38e605de184
Michail <vonamorm@mail.ru>
2023-12-28 14:03:53 +0300

    a

--- 8-strings/calc_expresson/calc_expression_value.h
+++ 8-strings/calc_expresson/calc_expression_value.h
@@ -1,6 +1,39 @@
 #include <math.h>
 #include <string.h>
 
-int calculate_expression(char expression[]){
-    return 0;
+int calculate_expression(char expression[])
+{
+    char *end;
+    long result = strtol(expression, &end, 10);
+
+    if (end == expression) 
+    {
+        return -1;
+    }
+
+    while (*end != '\0') 
+    {
+        long number = strtol(end + 1, &end, 10);
+
+        if (end == expression + 1) 
+        {
+            return -1;
+        }
+
+        if (*(end - 1) == '+') 
+        {
+            result += number;
+        } 
+        else if (*(end - 1) == '-') 
+        {
+            result -= number;
+        } 
+        else 
+        {
+            return -1;
+        }
+    }
+    
+    printf("%d", result);
+    return result;
 }
--- 8-strings/double_to_str/double_to_str_convert.h
+++ 8-strings/double_to_str/double_to_str_convert.h
@@ -1,6 +1,24 @@
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char* convert(double number){
-    return NULL;
+char* convert(double number) {
+    char* str = (char*) malloc(50 * sizeof(char));
+    sprintf(str, "%f", number);
+
+    char* dot_position = strchr(str, '.');
+    if (dot_position != NULL) 
+    {
+        char* end = str + strlen(str) - 1;
+        while (end > dot_position && *end == '0') 
+        {
+            *end-- = '\0';
+        }
+        if (*end == '.') 
+        {
+            *end = '\0';
+        }
+    }
+
+    return str;
 }
